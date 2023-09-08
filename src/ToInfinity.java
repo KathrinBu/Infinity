ad47a28a422201f0682ff7c5ea1b4172365cbd0d
Kate Bugrova <bugrovaev@mail.ru>
2023-09-08 20:35:28 +0300

    Initial commit

---
+++ src/ToInfinity.java
@@ -0,0 +1,11 @@
+public class ToInfinity {
+    public static void main(String[] args) {
+        int a=Integer.MAX_VALUE-1;
+        for (int i = a; i <=a+1 ; i++) {
+            System.out.println(i);
+            //будет написан комментарий который ниже
+            // \u000d System.out.println("комментарий будет выполнен!");
+
+        }
+    }
+}
